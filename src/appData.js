b396bc264aa639d68a04cd82bfe331a012521a9f
Sylvano Jonni <nokromanx@gmail.com>
2024-02-18 00:51:11 +0700

    replacing all the data needed in one file

---
+++ src/appData.js
@@ -0,0 +1,98 @@
+const user = [
+  {
+    img: '/stacy-testimonial.png',
+    id: 'stcy38',
+    name: 'Stacy',
+    age: 38,
+    weightLoss: 22,
+    monthLoss: 4.5,
+    description:
+      "Since Juniper I feel freedom. I've gone from always thinking about food, to seeing it as fuel.",
+  },
+  {
+    img: '/rebekah-testimonial.png',
+    id: 'rebkah27',
+    name: 'Rebekah',
+    age: 27,
+    weightLoss: 17,
+    monthLoss: 3.5,
+    description:
+      'I finally feel confident in my own skin. I feel pretty, and it feels great.',
+  },
+  {
+    img: '/jo-testimonial.png',
+    id: 'jo52',
+    name: 'Jo',
+    age: 52,
+    weightLoss: 12,
+    monthLoss: 3,
+    description: "I cannot believe how much better I feel. I'm amazed.",
+  },
+  {
+    img: '/tiffani-testimonial.png',
+    id: 'tiff52',
+    name: 'Tiffani',
+    age: 52,
+    weightLoss: 13,
+    monthLoss: 3.5,
+    description:
+      "I'm still on my journey, but this time I know I'll make it to the finish line.",
+  },
+];
+
+const post = [
+  {
+    id: 'nvwoine12934NKSAckl',
+    img: '/juniper-medication-product.png',
+    heading: 'Breakthrough medication',
+    title: 'Lowers appetite and improves metabolic function',
+  },
+  {
+    id: '09jkdvbwejpt3qcqwef',
+    img: '/juniper-tracking-indicator.svg',
+    heading: 'Health tracking',
+    title: 'Measure your biometric health improvements',
+  },
+  {
+    id: '02dikc4023gsv3t312vx',
+    img: '/juniper-community.png',
+    heading: 'Private community',
+    title: 'Celebrate successes and keep each other motivated',
+  },
+  {
+    id: '20mcph923bkalckp2hjtr2',
+    img: '/juniper-coaching.png',
+    heading: 'Lifestyle coaching',
+    title: 'Set the sustainable habits necessary for lifelong results',
+  },
+];
+
+const profile = [
+  {
+    img: '/Dr-Ramy-Bishay.png',
+    position: 'Endocrinologist',
+    name: 'Dr Ramy Bishay',
+    level: 'BSc (Hons), MSc (Hons), MBBS (Hons), FRACP',
+  },
+  {
+    img: '/Dr-Matthew-Vickers.png',
+    position: 'Specialist GP',
+    name: 'Dr Matthew Vickers',
+    level: 'MBBS, FRACGP',
+  },
+  {
+    img: '/Leif-Lagesen.png',
+    position: 'Health Coach',
+    name: 'Leif Lagesen',
+    level: 'APD, BSND (Hons), BSportExM',
+  },
+  {
+    img: '/Amelia-Bandera.png',
+    position: 'Dietian',
+    name: 'Amelia Bandera',
+    level: 'APD, Bex&NutrSci, MNutr&Diet',
+  },
+];
+
+
+export { user, post, profile };
