e85c6df39b91d2db4ce1ac6e4c4dd5754ee84098
Sylvano Jonni <nokromanx@gmail.com>
2024-02-18 12:26:10 +0700

    removing the comment in PeopleProfile component

--- src/components/PeopleProfile.jsx
+++ src/components/PeopleProfile.jsx
@@ -1,37 +1,3 @@
-// /* eslint-disable react/prop-types */
-// export default function PeopleProfile({
-//   position = 'Endocrinologist',
-//   name = 'Nok',
-//   level = 'MBA',
-//   img = '/Dr-Ramy-Bishay.png',
-// }) {
-//   return (
-//     <>
-//       <div>
-//         <div className="relative">
-//           <img
-//             className="rounded-lg"
-//             src={img}
-//             alt={`${name} picture`}
-//             width={380}
-//           />
-//         </div>
-//         <div className="absolute right-1">
-//           <p className="inline text-white font-medium bg-deep-maroon px-3 py-2 rounded-full">
-//             {position}
-//           </p>
-//         </div>
-//         <div>
-//           <h4 className="text-xl pb-2">{name}</h4>
-//         </div>
-//         <div>
-//           <p className="text-lg">{level}</p>
-//         </div>
-//       </div>
-//     </>
-//   );
-// }
-
 /* eslint-disable react/prop-types */
 export default function PeopleProfile({
   position = 'Endocrinologist',
