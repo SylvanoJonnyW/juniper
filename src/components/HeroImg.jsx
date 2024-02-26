d5eb8c569080f97dad5e9e802e5ee30c469f53eb
Sylvano Jonni <nokromanx@gmail.com>
2024-02-26 20:28:04 +0700

    modified each image size to fix the desktop layout width

--- src/components/HeroImg.jsx
+++ src/components/HeroImg.jsx
@@ -7,16 +7,18 @@ export default function Heroimg() {
             <img
               src="/hero-juniper-indicator.png"
               alt="Juniper stats indicator of weightloss"
+              width={340}
             />
           </div>
           <div>
-            <img src="/hero-juniper-component.png" alt="Pink Molecule" />
+            <img src="/hero-juniper-component.png" alt="Pink Molecule"  width={340}/>
           </div>
         </div>
         <div>
           <img
             src="/hero-juniper-face.png"
             alt="Juniper Brand Amabasador holding Juniper Program Package"
+            width={396}
           />
         </div>
       </div>
