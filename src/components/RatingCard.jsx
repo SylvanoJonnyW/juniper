9cbf49b9d2a75c5d6f8c1e27ee35e56f76f03a3c
Sylvano Jonni <nokromanx@gmail.com>
2024-02-27 00:50:18 +0700

    changing the children with props of description instead

--- src/components/RatingCard.jsx
+++ src/components/RatingCard.jsx
@@ -1,14 +1,14 @@
 /* eslint-disable react/prop-types */
 import Rating from './Rating';
 
-export default function RatingCard({ title, children }) {
+export default function RatingCard({ title, description }) {
   return (
     <>
       <div className="bg-white rounded-xl border-stroke border-2 w-fit h-fit">
         <div className="flex flex-col gap-6 p-4">
           <Rating />
           <p className="text-2xl text-primary font-semibold">{title}</p>
-          <p className="text-xl text-primary w-[340px]">{children}</p>
+          <p className="text-xl text-primary w-[340px]">{description}</p>
         </div>
       </div>
     </>
