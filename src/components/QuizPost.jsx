d8093d3cd669617b0ed9026d50b4b7713006b4aa
Sylvano Jonni <nokromanx@gmail.com>
2024-02-26 22:25:44 +0700

    tweaking the component layout & classes

--- src/components/QuizPost.jsx
+++ src/components/QuizPost.jsx
@@ -4,35 +4,35 @@ import Button from './Button';
 export default function QuizPost({ title, p1, p2 }) {
   return (
     <>
-      <div className="bg-white border-2 border-stroke w-fit rounded-lg">
-        <div className="p-10">
-          <h2 className=" text-4xl text-primary font-bold w-[320px] pb-4">
+      <div className="bg-white border-2 border-stroke w-[880px] h-[607px] rounded-lg">
+        <div className="p-12">
+          <h2 className=" text-6xl text-primary font-bold w-[420px] pb-6">
             {title}
           </h2>
-          <div className="w-[510px]">
-            <div className="text-primary pb-4 text-lg">
+          <div className="w-[560px]">
+            <div className="text-primary pb-6 text-2xl">
               <span>{p1} </span>
               <span className="font-bold">
                 lose over 13% of their body weight in one year.
               </span>
             </div>
-            <div className="pb-6">
-              <p className="text-primary text-lg">
+            <div className="pb-16">
+              <p className="text-primary text-2xl">
                 {p2} <sup>[1]</sup>
               </p>
             </div>
           </div>
-          <div className="flex gap-4 justify-between">
+          <div className="flex justify-between">
             <Button
               href={'take-quiz'}
-              className={'bg-primary text-white font-semibold px-20'}
+              className={'bg-primary text-white font-semibold px-36'}
             >
               Take the quiz
             </Button>
             <Button
               href={'juniper-weight-reset-program'}
               className={
-                'bg-white border-tertiary border-2 font-semibold text-primary px-20'
+                'bg-white border-tertiary border-2 font-semibold text-primary px-36'
               }
             >
               Learn More
