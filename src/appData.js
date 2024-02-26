0e09f72b7340ad10f8494a6ee6618ac4c06d6bbf
Sylvano Jonni <nokromanx@gmail.com>
2024-02-27 00:49:03 +0700

    creating new entry for RatingCard component

--- src/appData.js
+++ src/appData.js
@@ -46,28 +46,28 @@ const post = [
     img: '/juniper-medication-product.png',
     heading: 'Breakthrough medication',
     title: 'Lowers appetite and improves metabolic function',
-    to: 'medication-blog'
+    to: 'medication-blog',
   },
   {
     id: '09jkdvbwejpt3qcqwef',
     img: '/juniper-tracking-indicator.svg',
     heading: 'Health tracking',
     title: 'Measure your biometric health improvements',
-    to: 'health-tracking-blog'
+    to: 'health-tracking-blog',
   },
   {
     id: '02dikc4023gsv3t312vx',
     img: '/juniper-community.png',
     heading: 'Private community',
     title: 'Celebrate successes and keep each other motivated',
-    to: 'juniper-private-comunity'
+    to: 'juniper-private-comunity',
   },
   {
     id: '20mcph923bkalckp2hjtr2',
     img: '/juniper-coaching.png',
     heading: 'Lifestyle coaching',
     title: 'Set the sustainable habits necessary for lifelong results',
-    to: 'lifestyle-coaching'
+    to: 'lifestyle-coaching',
   },
 ];
 
@@ -139,4 +139,31 @@ const navigation = [
   },
 ];
 
-export { user, post, profile, quizPost, navigation };
+const rating = [
+  {
+    id: 'nio351nvc135ncdi23',
+    title: 'Very happy with my weight loss',
+    description:
+      "I'm really glad I took the leap and did this for myself, highly recommend you to try it out if you're on the fence.",
+  },
+  {
+    id: '29chjrg3h34b20xj336t',
+    title: 'I am loving the Juniper Program',
+    description:
+      'I am loving the Juniper program, finally I have found something that actually helps me lose weight. In the first couple of weeks i have lost 4 kilos. Support is great too.',
+  },
+  {
+    id: '120cj4533inasnqitribs',
+    title: 'I wish I found this program earlier',
+    description:
+      "The support is so much better than my regular GP. I can't wait to receive my welcome package and start my journey through Juniper.",
+  },
+  {
+    id: '19u4cmeuw3ur12xnmbg2jkkq',
+    title: 'Really greatful to have found this',
+    description:
+      'I had done my research and I am very happy with the service and product. It is working great for me.',
+  },
+];
+
+export { user, post, profile, quizPost, navigation, rating };
